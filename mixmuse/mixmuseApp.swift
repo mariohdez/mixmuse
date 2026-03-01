12affb5ab11a48d5faf4ae110c92b8d3d9c31be9
Mario Hernandez <mhmario963@gmail.com>
2026-02-28 20:40:56 -0500

    reformat code.

--- mixmuse/mixmuseApp.swift
+++ mixmuse/mixmuseApp.swift
@@ -5,19 +5,30 @@
 //  Created by Mario Hernandez on 2/28/26.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct mixmuseApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Item.self
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        #if DEBUG
+            let isDev = true
+        #else
+            let isDev = false
+        #endif
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: isDev
+        )
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return try ModelContainer(
+                for: schema,
+                configurations: [modelConfiguration]
+            )
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
