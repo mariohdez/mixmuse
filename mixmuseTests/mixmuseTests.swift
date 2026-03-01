79c0c79c74f23d38abd932ea370cf717db136e58
Mario Hernandez <mhmario963@gmail.com>
2026-02-28 19:12:46 -0500

    Initial Commit

---
+++ mixmuseTests/mixmuseTests.swift
@@ -0,0 +1,17 @@
+//
+//  mixmuseTests.swift
+//  mixmuseTests
+//
+//  Created by Mario Hernandez on 2/28/26.
+//
+
+import Testing
+@testable import mixmuse
+
+struct mixmuseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
